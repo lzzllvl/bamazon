7e31b87f1c641beaedf16af87e67f7be88b6ad8b
Greg Lee <gml2136@gmail.com>
2017-04-09 20:11:52 -0700

    started work on the customer app

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,17 @@
 const DBConnection = require('./dbConnection.js');
 const prompt = require('prompt');
+
+DBConnection.prototype.displayProducts = function(){
+  let conn = this.connection;
+  conn.query("SELECT * FROM products", function(err, res){
+    if(err) throw err;
+    console.log(res);
+  });
+};
+
+
+const customer = {
+
+}
+
+module.exports = customer; 
